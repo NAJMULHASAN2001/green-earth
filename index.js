8123387a5e5e4391f07870e521f772510a5b2eab
NAJMULHASAN001 <najmulhasan2001@gmail.com>
2026-02-04 10:32:13 +0600

    added footer

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 console.log('connected')
 const categoryContainer = document.getElementById('category-container')
-const cardContainer =document.getElementById('card-container')
-const alltrees =document.getElementById('all-trees')
+const cardContainer = document.getElementById('card-container')
+const alltrees = document.getElementById('all-trees')
 
 //load every trees
-const loadAllTrees =() =>{
+const loadAllTrees = () => {
     fetch('https://openapi.programming-hero.com/api/plants')
-    .then(res => res.json())
-    .then(data => {
-        const allPlants = data.plants
-        displayAllTrees(allPlants)
-    })
+        .then(res => res.json())
+        .then(data => {
+            const allPlants = data.plants
+            displayAllTrees(allPlants)
+        })
 }
 loadAllTrees()
 
-const displayAllTrees= (allPlants)=>{
-    allPlants.forEach(plant =>{
-        console.log(plant)
-        cardContainer.innerHTML +=`
+const displayAllTrees = (allPlants) => {
+    allPlants.forEach(plant => {
+        // console.log(plant)
+        cardContainer.innerHTML += `
         <div class="card bg-base-100 w-80 shadow-sm mb-10 mx-auto">
                     <figure>
                         <img class="h-[250px] w-[300px]" src="${plant.image}"
                             alt="Shoes" />
                     </figure>
                     <div class="space-y-2 p-3">
-                        <h2 class="card-title mt-3">${plant.name}</h2>
+                        <h2 onclick="my_modal_1.showModal() class="card-title mt-3">${plant.name}</h2>
                         <p>${plant.description}
                         </p>
                         <div class="flex justify-between">
@@ -32,7 +32,7 @@ const displayAllTrees= (allPlants)=>{
                         <p class="plant-price">৳ ${plant.price}</p>
                         </div>
                         
-                        <button  class="btn add-cart-btn bg-[#15803D] text-white w-full">Add To Cart</button>
+                        <button class="btn add-cart-btn bg-[#15803D] text-white w-full">Add To Cart</button>
                         
                     </div>
         `
@@ -62,13 +62,13 @@ const displayCategory = (categories) => {
 
 
     categoryContainer.addEventListener('click', (e) => {
-        const allP =document.querySelectorAll('p')
+        const allP = document.querySelectorAll('p')
 
-        allP.forEach(p =>{
+        allP.forEach(p => {
             p.classList.remove('bg-[#CFF0DC]')
-        } )
+        })
 
-        if(e.target.localName ==="p"){
+        if (e.target.localName === "p") {
             // console.log(e.target.id)
             e.target.classList.add('bg-[#CFF0DC]')
             loadTreesByCategory(e.target.id)
@@ -77,31 +77,31 @@ const displayCategory = (categories) => {
 }
 
 //load trees by category
-const loadTreesByCategory =(categoryId)=>{
+const loadTreesByCategory = (categoryId) => {
     // console.log(categoryId)
     fetch(`https://openapi.programming-hero.com/api/category/${categoryId}`)
-    .then(res => res.json())
-    .then(data => {
-        displayTreeByCategory(data.plants)
-    })
-    .catch(err =>{
-        console.log(err)
-    })
+        .then(res => res.json())
+        .then(data => {
+            displayTreeByCategory(data.plants)
+        })
+        .catch(err => {
+            console.log(err)
+        })
 }
 
 
-const displayTreeByCategory =(plants) =>{
-    cardContainer.innerHTML =''
-    plants.forEach(plant=>{
+const displayTreeByCategory = (plants) => {
+    cardContainer.innerHTML = ''
+    plants.forEach(plant => {
         console.log(plant)
-        cardContainer.innerHTML +=`
+        cardContainer.innerHTML += `
                         <div class="card bg-base-100 w-80 shadow-sm mb-10 mx-auto">
                     <figure>
                         <img class="h-[250px] w-[300px]" src="${plant.image}"
                             alt="Shoes" />
                     </figure>
                     <div class="space-y-2 p-3">
-                        <h2 class="card-title mt-3">${plant.name}</h2>
+                        <h2 onclick="my_modal_1.showModal()  class="card-title mt-3">${plant.name}</h2>
                         <p>${plant.description}
                         </p>
                         <div class="flex justify-between">
@@ -113,19 +113,24 @@ const displayTreeByCategory =(plants) =>{
                         
                     </div>
         `
+
+    cardContainer.addEventListener('click', (e) => {
+    if (e.target.classList.contains('add-cart-btn')) {
+        const getContainer = document.getElementById('cart-container')
+        getContainer.innerHTML += `
+         <div class="bg-[#F0FDF4] mb-2 p-2">
+                        <h1>${plant.name}</h1>
+                        <p>${plant.price} x 1</p>
+                    </div>
+        `
+    }
+})
     })
 
 }
-//----------------------add event listener--------------------
-const cartBtns = document.getElementsByClassName('add-cart-btn')
-const plantPrices = document.getElementsByClassName('plant-price')
-// const total =
-for (let cartBtn of cartBtns ){
-    cartBtn.addEventListener("click", function(){
-        const getTitle =cartBtn
-        console.log(getTitle)
-    })
-}
+
+
+
 
 
 
